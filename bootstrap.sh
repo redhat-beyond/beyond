34dcb4f1d4b00836c00633ffe6139daf326b4585
Ilan Zuckerman <izuckerm@redhat.com>
2020-02-18 10:42:49 +0200

    provisioning virtualenv + virtualenvwrapper

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,18 +2,16 @@
 
 apt-get update -y
 apt-get install python3-pip -y
-pip3 install --yes virtualenv
-pip3 install --yes virtualenvwrapper
+yes | pip3 install virtualenv
+yes | pip3 install virtualenvwrapper
 
-# make directory for virtual envs
-mkdir ~/.virtualenvs
+# make directory for virtual envs. dont fail if it already exists + change permissions to vagrant user
+mkdir -p /home/vagrant/.virtualenvs
+chown -R vagrant:vagrant /home/vagrant/.virtualenvs
 
 # set WORKON_HOME to our virtualenv dir
-export WORKON_HOME=~/.virtualenvs
+echo "export WORKON_HOME=/home/vagrant/.virtualenvs" >> /home/vagrant/.bashrc
 
 # add this to .bashrc so that the virtualenvwrapper commands are loaded.
-. /usr/local/bin/virtualenvwrapper.sh
-
-# reload .bashrc and we are ready to go!
-. .bashrc
-
+echo "VIRTUALENVWRAPPER_PYTHON='/usr/bin/python3'" >> /home/vagrant/.bashrc
+echo "source /usr/local/bin/virtualenvwrapper.sh" >> /home/vagrant/.bashrc
