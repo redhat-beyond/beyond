dc1e00b6d9a2ab3cb37b8d3b712d4af74c4b4c94
Ilan Zuckerman <izuckerm@redhat.com>
2020-02-18 09:37:22 +0200

    Populating virtualenvwrapper via bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 apt-get update -y
+apt-get install python3-pip -y
+pip3 install --yes virtualenv
+pip3 install --yes virtualenvwrapper
+
+# make directory for virtual envs
+mkdir ~/.virtualenvs
+
+# set WORKON_HOME to our virtualenv dir
+export WORKON_HOME=~/.virtualenvs
+
+# add this to .bashrc so that the virtualenvwrapper commands are loaded.
+. /usr/local/bin/virtualenvwrapper.sh
+
+# reload .bashrc and we are ready to go!
+. .bashrc
+
