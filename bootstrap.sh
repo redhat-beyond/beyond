147ebedcba6227d71499a6080d1577bc857c6c0d
Ilan Zuckerman <izuckerm@redhat.com>
2020-03-23 13:10:34 +0200

    adding custom path for the requirements

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,13 @@
 
 apt-get update -y
 apt-get install python3-pip -y
-pip3 install -r /vagrant/requirements.txt
-nohup python3 /vagrant/app.py runserver & sleep 1
+
+if [ $TRAVIS ]
+then
+   my_path=$TRAVIS_BUILD_DIR
+else
+   my_path='/vagrant'
+fi
+
+pip3 install -r $my_path/requirements.txt
+nohup python3 $my_path/app.py runserver & sleep 1
