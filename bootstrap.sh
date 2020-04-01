5221db6fda3a4e0b5059415611793186e2455a2e
Ilan Zuckerman <izuckerm@redhat.com>
2020-04-01 09:17:17 +0300

    added github actions file

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,12 +3,13 @@
 apt-get update -y
 apt-get install python3-pip -y
 
-if [ $TRAVIS ]
-then
-   my_path=$TRAVIS_BUILD_DIR
-else
-   my_path='/vagrant'
-fi
+#if [ $TRAVIS ]
+#then
+#   my_path=$TRAVIS_BUILD_DIR
+#else
+#   my_path='/vagrant'
+#fi
 
-pip3 install -r $my_path/requirements.txt
-nohup python3 $my_path/app.py runserver & sleep 1
+pwd
+pip3 install -r /vagrant/requirements.txt
+nohup python3 /vagrant/app.py runserver & sleep 1
